9ad0100940f6013415f5c72315538b7df362d074
hugosenari <hugosenari@gmail.com>
2022-03-22 01:08:27 -0300

    chore: re-index ../nimpkgs/d/datamancer

--- v0_2_2/flake.nix
+++ v0_2_2/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-datamancer-v0_2_2.flake = false;
+  inputs.src-datamancer-v0_2_2.ref   = "refs/tags/v0.2.2";
   inputs.src-datamancer-v0_2_2.owner = "SciNim";
-  inputs.src-datamancer-v0_2_2.ref   = "v0_2_2";
   inputs.src-datamancer-v0_2_2.repo  = "datamancer";
   inputs.src-datamancer-v0_2_2.type  = "github";
   
