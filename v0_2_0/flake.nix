234229310a6aaa1b408c43344adddb249e7c146c
hugosenari <hugosenari@gmail.com>
2022-03-28 03:01:16 -0300

    chore: re index datamancer

--- v0_2_0/flake.nix
+++ v0_2_0/flake.nix
@@ -13,13 +13,13 @@
   inputs.src-datamancer-v0_2_0.repo  = "datamancer";
   inputs.src-datamancer-v0_2_0.type  = "github";
   
-  inputs."github.com/vindaar/seqmath".owner = "nim-nix-pkgs";
-  inputs."github.com/vindaar/seqmath".ref   = "master";
-  inputs."github.com/vindaar/seqmath".repo  = "github.com/vindaar/seqmath";
-  inputs."github.com/vindaar/seqmath".dir   = "";
-  inputs."github.com/vindaar/seqmath".type  = "github";
-  inputs."github.com/vindaar/seqmath".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."github.com/vindaar/seqmath".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  inputs."github-vindaar-seqmath".owner = "nim-nix-pkgs";
+  inputs."github-vindaar-seqmath".ref   = "master";
+  inputs."github-vindaar-seqmath".repo  = "github-vindaar-seqmath";
+  inputs."github-vindaar-seqmath".dir   = "v0_1_13";
+  inputs."github-vindaar-seqmath".type  = "github";
+  inputs."github-vindaar-seqmath".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."github-vindaar-seqmath".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
   inputs."arraymancer".owner = "nim-nix-pkgs";
   inputs."arraymancer".ref   = "master";
