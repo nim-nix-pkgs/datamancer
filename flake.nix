164c83fdf2321d1a8f1657b4031b54e6663776d4
hugosenari <hugosenari@gmail.com>
2022-08-05 13:40:15 -0300

    chore: re index datamancer

--- flake.nix
+++ flake.nix
@@ -215,6 +215,14 @@
   inputs."datamancer-v0_3_3".inputs.nixpkgs.follows = "nixpkgs";
   inputs."datamancer-v0_3_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
+  inputs."datamancer-v0_3_4".dir   = "v0_3_4";
+  inputs."datamancer-v0_3_4".owner = "nim-nix-pkgs";
+  inputs."datamancer-v0_3_4".ref   = "master";
+  inputs."datamancer-v0_3_4".repo  = "datamancer";
+  inputs."datamancer-v0_3_4".type  = "github";
+  inputs."datamancer-v0_3_4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."datamancer-v0_3_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
